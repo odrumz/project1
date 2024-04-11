4d9395777bc67c553f9e8449ff70a158ab34aee7
DerrickOngoma <Odrumz64@gmail.com>
2024-04-11 04:24:29 +0300

    adding empty functions

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ const apikey = "61a740118d162c882bcaaa27e5123b0b"
 weatherform.addEventListener("submit",  (e) => {
     e.preventDefault();
 })
+async function getweatherData(city){
+ 
+}
+function displayweatherinfo(data){
+}
+function getweatherEmoji(weatherId){
+   
+
+}
+function displayerror(message){
+    
+}
