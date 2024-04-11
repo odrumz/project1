47c0b87bffad94df6abad7a4b937566849d751f1
DerrickOngoma <Odrumz64@gmail.com>
2024-04-11 03:57:31 +0300

    begining js

---
+++ index.js
@@ -0,0 +1,5 @@
+const weatherform = document.querySelector(".weatherform");
+const weatherdata = document.querySelector(".weatherdata")
+const weatherbox = document.querySelector(".weatherbox")
+const apikey = "61a740118d162c882bcaaa27e5123b0b"
+
